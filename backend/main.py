f08adeeaeb7d5eda302123574dd1875d98291bcb
code-sharingan <87212102+code-sharingan@users.noreply.github.com>
2024-01-31 00:21:14 -0700

    updated main
    added front page for  different documentation requests

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI,request
+from fastapi import FastAPI
 
 app = FastAPI(
     title="Pony Express Chat",
     description="api for chatting",
     version="0.1.0")
 
+
+# added this default loading page to take to different documentations
+@app.get("/", include_in_schema=False)
+def default() -> str:
+    return HTMLResponse(
+        content=f"""
+        <html>
+            <body>
+                <h1>{app.title}</h1>
+                <p>{app.description}</p>
+                <h2>API docs</h2>
+                <ul>
+                    <li><a href="/docs">Swagger</a></li>
+                    <li><a href="/redoc">ReDoc</a></li>
+                </ul>
+            </body>
+        </html>
+        """,
+    )
 
