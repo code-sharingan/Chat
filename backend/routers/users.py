447a30bbd8553cc3a0f42bcef5e208ff369d5540
code-sharingan <87212102+code-sharingan@users.noreply.github.com>
2024-02-01 03:03:12 -0700

    fixed the chat not being sorted bug

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -32,7 +32,8 @@ def create_user(user_create: userCreate):
 
 @user_router.get("/{user_id}/chats")
 def get_user_chats(user_id:str)->chatCollection:
+    sort_key = lambda Chat: Chat.name
     if(not db.user_exists(user_id)):
         raise HTTPException(status_code =404 , detail={"detail":{"type":"entity_not_found" , "entity_name":"User","entity_id":user_id}})
     chats_list = db.get_user_chats(user_id)
-    return chatCollection(meta={"count":len(chats_list)} , chats=chats_list)
+    return chatCollection(meta={"count":len(chats_list)} , chats=sorted(chats_list,key =sort_key))
