023e81fd83be991d91f4f72e6531832c7f8e2702
code-sharingan <87212102+code-sharingan@users.noreply.github.com>
2024-02-04 18:08:49 -0700

    added descriptiond for all the routers

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -8,7 +8,7 @@ from backend.entities import UserCollection,User,userCreate,userIDB,chatCollecti
 
 user_router = APIRouter(prefix="/users",tags=["users"])
 
-@user_router.get("",response_model=UserCollection)
+@user_router.get("",response_model=UserCollection,description="this api call returns all the users in the database")
 def get_users():
     sort_key=lambda user: user.id
     users = db.get_all_users()
@@ -23,14 +23,14 @@ def get_user(user_id:str):
     return userIDB(user=db.get_user_by_id(user_id))
 
 
-@user_router.post("",response_model = userIDB)
+@user_router.post("",response_model = userIDB ,description="this api call adds the particular user to the database")
 def create_user(user_create: userCreate):
     if(db.user_exists(user_create.id)):
         raise HTTPException(status_code =422 , detail={"detail":{"type":"duplicate_entity" , "entity_name":"User","entity_id":user_create.id}})
     return userIDB(user=db.create_user(user_create))
 
 
-@user_router.get("/{user_id}/chats")
+@user_router.get("/{user_id}/chats", response_model= chatCollection,description="this api9 call gets all the chats in which the partticular user is an active participant")
 def get_user_chats(user_id:str)->chatCollection:
     sort_key = lambda Chat: Chat.name
     if(not db.user_exists(user_id)):
